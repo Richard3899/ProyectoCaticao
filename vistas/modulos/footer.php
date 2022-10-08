8757e3812ad26f6e8d4930c802750339e4c0d4e9
Richard <richard.arias3899@gmail.com>
2022-10-07 22:56:02 -0500

    Avance de plantilla

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,22 @@
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+
+<!-- jQuery -->
+<script src="vistas/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="vistas/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="vistas/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="vistas/dist/js/demo.js"></script>
