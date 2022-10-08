d2a9afd688956b9be28a05f1f388f2180bfe1507
Richard <richard.arias3899@gmail.com>
2022-10-07 22:57:26 -0500

    Edicion de plantilla

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,26 @@
+<header class="main-header">
+
+<a href="" class="logo">
+
+    <!– LOGO MINI –>
+    <span class="logo-mini">
+        <img src="vistas/img/plantilla/logo-mini.jpg" class="img-responsive" style="padding:10px">
+    </span>
+
+    <!– LOGO NORMAL–>
+    <span class="logo-mini">
+        <img src="vistas/img/plantilla/logo.jpg" class="img-responsive" style="padding:10px 0px">
+    </span>
+</a>
+
+ <!-- Navbar -->
+ <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      
+     
+    </ul>
+
+</header>
---
+++ vistas/plantilla copy 2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Caticao</title>
+
+   <!– PLUGINS DE CSS –>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="vistas/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="vistas/dist/css/adminlte.css">
+ 
+  <!– PLUGINS DE JAVASCRIPT –>
+
+  <!-- jQuery -->
+  <script src="vistas/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="vistas/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="vistas/dist/js/adminlte.min.js"></script>
+</head>
+
+<!– CUERPO DOCUMENTO –>
+<body class="hold-transition sidebar-collapse sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+
+<?php 
+
+include "modulos/cabezote.php";
+
+?>
+</div>
+<!-- ./wrapper -->
+
+<script src="vistas/js/plantilla.js"></script>
+
+</body>
+</html>
