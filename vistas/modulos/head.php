fb8f6ff09eecea63c2cdbe5b2ff5fe452d6b644c
Richard <richard.arias3899@gmail.com>
2022-10-13 19:47:36 -0500

    Cambio de titulo

--- vistas/modulos/head.php
+++ vistas/modulos/head.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Blank Page</title>
+  <title>CATICAO</title>
   <link rel="icon" href="vistas/img/plantilla/icono-chocolate.png">
 
   <!-- Google Font: Source Sans Pro -->
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <?php 
 
