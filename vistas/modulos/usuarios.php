de42e030dac544feeb2660f3ce2cb9e9afe485d8
keyla0210 <keyladionisio@gmail.com>
2022-10-15 00:41:34 -0500

    usuario

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -1,20 +1,110 @@
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-12">
-            <h1>Administrador de Usuarios</h1>
-          </div>
+<div class="content-wrapper">
+  <section class="content-header">
+    <h1>
+      Administrar usuarios
+    </h1>
+    <ol class="breadcrumb">
+      
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Administrar usuarios</li>
+    
+    </ol>
+  </section>
+  <section class="content">
+    <div class="box">
 
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+      <div class="box-header with-border">
 
-    <!-- Main content -->
-    <section class="content">
+       <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
+      
+        Agregar usuario
 
+        </button>
+
+      </div>
+
+      <div class="box-body">
+
+       <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
+ 
+        <thead>
+
+         <tr>
+            <th style="width:10px">#</th>
+            <th>Nombre</th>
+            <th>Usuario</th>
+            <th>Foto</th>
+            <th>Perfil</th>
+            <th>Estado</th>
+            <th>Último login</th>
+            <th>Acciones</th>
+
+          
+          </tr> 
+
+        </thead>
+
+        <tbody>
+
+        <?php
+        $item = null;
+        $valor = null;
+
+        $usuarios = ControladorUsuarios::ctrMostrarUsuarios($item, $valor);
+
+       foreach ($usuarios as $key => $value){
+  
+        echo ' <tr>
+               <td>1</td>
+               <td>'.$value["nombre"].'</td>
+               <td>'.$value["usuario"].'</td>';
+
+                if($value["foto"] != ""){
+
+                 echo '<td><img src="'.$value["foto"].'" class="img-thumbnail" width="40px"></td>';
+
+                }else{
+
+                 echo '<td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>';
+
+                }
+
+                 echo '<td>'.$value["perfil"].'</td>';
+
+                if($value["estado"] != 0){
+
+                 echo '<td><button class="btn btn-success btn-xs btnActivar" idUsuario="'.$value["id"].'" estadoUsuario="0">Activado</button></td>';
+
+                }else{
+
+                 echo '<td><button class="btn btn-danger btn-xs btnActivar" idUsuario="'.$value["id"].'" estadoUsuario="1">Desactivado</button></td>';
+
+                }             
+
+                echo '<td>'.$value["ultimo_login"].'</td>
+                <td>
+
+                  <div class="btn-group">
+                    
+                    <button class="btn btn-warning btnEditarUsuario" idUsuario="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarUsuario"><i class="fa fa-pencil"></i></button>
+
+                    <button class="btn btn-danger btnEliminarUsuario" idUsuario="'.$value["id"].'" fotoUsuario="'.$value["foto"].'" usuario="'.$value["usuario"].'"><i class="fa fa-times"></i></button>
+
+                  </div>  
+                </td>
+
+               </tr>';
+        }
+        ?> 
+        </tbody>
+        </table>
+      </div>
+     </div>
+  </section>
+</div>
+
+</section>
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
@@ -42,5 +132,4 @@
 
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</div>
