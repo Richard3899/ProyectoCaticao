860acc346fd836b51522b79d13e3922ed76b4399
Richard <richard.arias3899@gmail.com>
2022-11-03 19:31:50 -0500

    p1

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -6,7 +6,7 @@ class Conexion{
 
 		$link = new PDO("mysql:host=localhost;dbname=caticao",
 			            "root",
-			            "root");
+			            "");
 
 		$link->exec("set names utf8");
 
@@ -15,4 +15,3 @@ class Conexion{
 	}
 
 }
-hola
